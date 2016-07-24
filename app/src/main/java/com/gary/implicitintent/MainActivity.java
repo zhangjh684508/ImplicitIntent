a797d89bc53441b9a45ddd8d109ef084a587b56a
zhangjh684508 <583887669@qq.com>
2016-07-24 09:33:42 +0800

    check intent existence

--- app/src/main/java/com/gary/implicitintent/MainActivity.java
+++ app/src/main/java/com/gary/implicitintent/MainActivity.java
@@ -1,11 +1,14 @@
 package com.gary.implicitintent;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,14 +33,16 @@ public class MainActivity extends AppCompatActivity {
 
                 Uri webPage=Uri.parse("http://www.bing.com");
                 Intent webIntent=new Intent(Intent.ACTION_VIEW, webPage);
-                startActivity(webIntent);
+                PackageManager packageManager=getPackageManager();
+                List activities=packageManager.queryIntentActivities(webIntent, PackageManager.MATCH_DEFAULT_ONLY);
 
+                if (activities.size()>0)
+                {
+                    startActivity(webIntent);
+                }
             }
         }
         );
-
-
-
 
     }
 }
